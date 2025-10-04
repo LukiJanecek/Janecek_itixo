06cd94fb866624dee1bd2d1bfc2396626847eaa7
Lukii <lukas.janecek.02@gmail.com>
2025-10-04 14:21:05 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Janecek_itixo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
